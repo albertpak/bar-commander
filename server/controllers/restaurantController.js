be0896043acec395240b9e7fe45c088aa72f78af
Jesús Ángel <jesus.gonzalez@wazuh.com>
2018-08-13 12:38:31 +0200

    Using async/await for restaurants

--- server/controllers/restaurantController.js
+++ server/controllers/restaurantController.js
@@ -10,115 +10,115 @@ module.exports = {
     /**
      * RestaurantController.list()
      */
-    list: function (req, res) {
-        RestaurantModel.find(function (err, restaurants) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting Restaurant.',
-                    error: err
-                });
-            }
+    list: async (req, res) => {
+        try {
+            const restaurants = await RestaurantModel.find();
+            
             return res.json({data: restaurants, error: 0});
-        });
+
+        } catch (error) {
+            return res.status(500).json({
+                code: 2000,
+                error: error.message || error
+            });
+        }
     },
 
     /**
      * RestaurantController.show()
      */
-    show: function (req, res) {
-        var id = req.params.id;
-        RestaurantModel.findOne({_id: id}, function (err, restaurant) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting Restaurant.',
-                    error: err
-                });
-            }
+    show: async (req, res) => {
+        try {
+            const { id } = req.params;
+
+            const restaurant = await RestaurantModel.findOne({_id: id});
+            
             if (!restaurant) {
-                return res.status(404).json({
-                    message: 'No such Restaurant'
-                });
+                throw Error('No such Restaurant');
             }
+
             return res.json({data: restaurant, error: 0});
-        });
+
+        } catch (error) {
+            return res.status(500).json({
+                code: 2001,
+                error: error.message || error
+            });
+        }
     },
 
     /**
      * RestaurantController.create()
      */
-    create: function (req, res) {
-        var Restaurant = new RestaurantModel({
-			name : req.body.name,
-			address : req.body.address,
-			orders : req.body.orders,
-			menu : req.body.menu,
-			owner : req.body.owner,
-			waiters : req.body.waiters
+    create: async (req, res) => {
+        try {
+            const restaurant = new RestaurantModel({
+                name : req.body.name,
+                address : req.body.address,
+                orders : req.body.orders,
+                menu : req.body.menu,
+                owner : req.body.owner,
+                waiters : req.body.waiters
+            });
+    
+            await restaurant.save();
 
-        });
-
-        Restaurant.save(function (err, restaurant) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when creating Restaurant',
-                    error: err
-                });
-            }
             return res.status(201).json({data: restaurant, error: 0});
-        });
+
+        } catch (error) {
+            return res.status(500).json({
+                code: 2002,
+                error: error.message || error
+            });
+        }
     },
 
     /**
      * RestaurantController.update()
      */
-    update: function (req, res) {
-        var id = req.params.id;
-        RestaurantModel.findOne({_id: id}, function (err, restaurant) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when getting Restaurant',
-                    error: err
-                });
-            }
+    update: async (req, res) => {
+        try {
+            const { id } = req.params;
+
+            const restaurant = await RestaurantModel.findOne({_id: id});
+
             if (!restaurant) {
-                return res.status(404).json({
-                    message: 'No such Restaurant'
-                });
+                throw Error('No such Restaurant');
             }
 
             restaurant.name = req.body.name ? req.body.name : restaurant.name;
-			restaurant.address = req.body.address ? req.body.address : restaurant.address;
-			restaurant.orders = req.body.orders ? req.body.orders : restaurant.orders;
-			restaurant.menu = req.body.menu ? req.body.menu : restaurant.menu;
-			restaurant.owner = req.body.owner ? req.body.owner : restaurant.owner;
-			restaurant.waiters = req.body.waiters ? req.body.waiters : restaurant.waiters;
-			
-            restaurant.save(function (err, restaurant) {
-                if (err) {
-                    return res.status(500).json({
-                        message: 'Error when updating Restaurant.',
-                        error: err
-                    });
-                }
+            restaurant.address = req.body.address ? req.body.address : restaurant.address;
+            restaurant.orders = req.body.orders ? req.body.orders : restaurant.orders;
+            restaurant.menu = req.body.menu ? req.body.menu : restaurant.menu;
+            restaurant.owner = req.body.owner ? req.body.owner : restaurant.owner;
+            restaurant.waiters = req.body.waiters ? req.body.waiters : restaurant.waiters;
+            
+            await restaurant.save();
 
-                return res.json({data: restaurant, error: 0});
+            return res.json({data: restaurant, error: 0});
+            
+        } catch (error) {
+            return res.status(500).json({
+                code: 2003,
+                error: error.message || error
             });
-        });
+        }
     },
 
     /**
      * RestaurantController.remove()
      */
-    remove: function (req, res) {
-        var id = req.params.id;
-        RestaurantModel.findByIdAndRemove(id, function (err, restaurant) {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Error when deleting the Restaurant.',
-                    error: err
-                });
-            }
+    remove: async (req, res) => {
+        try {
+            const { id } = req.params;
+            await RestaurantModel.findByIdAndRemove(id);
             return res.status(204).json();
-        });
+        } catch (error) {
+            return res.status(500).json({
+                code: 2004,
+                error: error.message || error
+            });
+        }
+
     }
 };
